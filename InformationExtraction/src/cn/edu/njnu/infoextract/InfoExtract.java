cdf0399dd76595d06729303f49b854aa577056bd
ZhangZhi <1557983850@qq.com>
2015-12-22 17:54:45 +0800

    update

--- InformationExtraction/src/cn/edu/njnu/infoextract/InfoExtract.java
+++ InformationExtraction/src/cn/edu/njnu/infoextract/InfoExtract.java
@@ -1,12 +1,19 @@
 package cn.edu.njnu.infoextract;
 
-import cn.edu.njnu.Main;
 import cn.edu.njnu.domain.Extractable;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -116,17 +123,7 @@ public abstract class InfoExtract {
      * @param item 待判断的字段
      * @return 是否是时间
      */
-    protected boolean canbeTime(String item) {
-        return false;
-    }
-
-    /**
-     * 判断一个字段项是否是地点
-     *
-     * @param item 待判断的字段
-     * @return 是否是地点
-     */
-    protected boolean canBePlace(String item) {
+    protected boolean canBeTime(String item) {
         Pattern p = Pattern.compile("(.*\\d{1,4}[-|\\/|年|\\.]\\d{1,2}" +
                 "[-|\\/|月|\\.]\\d{1,2}([日|号])?(\\s)*(\\d{1,2}([点|时])" +
                 "?((:)?\\d{1,2}(分)?((:)?\\d{1,2}(秒)?)?)?)?(\\s)*(PM|AM)?).*");
@@ -146,17 +143,77 @@ public abstract class InfoExtract {
     }
 
     /**
-     * 从dataList中提取出结构化信息
-     * <p>
-     * //* @return 所抽取的结构化信息
+     * 从给定的request地址发出请求并返回response字符串
      *
+     * @param request 请求的地址
+     * @return 返回response字符串
+     */
+    protected String getResponseFromRequest(String request) throws IOException {
+        //使用apache的开源网络工具箱请求语料库数据
+        HttpGet get = new HttpGet(request);
+        HttpClient http = new DefaultHttpClient();
+        HttpResponse response = http.execute(get);
+        if (response.getStatusLine().getStatusCode() == 200) {
+            HttpEntity entity = response.getEntity();
+            InputStream in = entity.getContent();
+            byte[] buffer = new byte[4096];
+            int hasread;
+            String data;
+            if ((hasread = in.read(buffer)) > 0) {
+                data = new String(buffer, 0, hasread);
+                return data;
+            } else
+                return null;
+        }
+        return null;
+    }
+
+    /**
+     * 从语料库返回的json字符串中提取可能的地址
+     *
+     * @param json 包含可能地址的json字符串
+     * @return 抽取的可能的地址的字符串
+     */
+    protected String[] getPosiblePlaces(String json) {
+        JSONObject jsonObj = JSONObject.fromObject(json);
+        JSONArray array = jsonObj.getJSONArray("results");
+        String[] places = new String[array.length()];
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject item = array.getJSONObject(i);
+            places[i] = item.getString("name");
+        }
+        return places;
+    }
+
+    /**
+     * 判断一个字段项是否是地点
+     *
+     * @param item 待判断的字段
+     * @param city 查找的城市
+     * @return 是否是一个地点
+     */
+    protected boolean canBePlace(String item, String city) throws IOException {
+        final String appkey = "dnHbgky1GB0HMRt7GReO0Sxp";
+        String request = "http://api.map.baidu.com/place/v2/search?" +
+                "q=" + item + "&region=" + city + "&output=json&ak=" + appkey;
+        String response = getResponseFromRequest(request);
+        String[] places;
+        if (response != null) {
+            places = getPosiblePlaces(response);
+            for (String place : places) {
+                if (item.equals(place))
+                    return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 从dataList中提取出结构化信息
+     *
+     * @return 所抽取的结构化信息
      * @throws IOException
      */
-    public void extractInformation(File file) throws IOException {
-        parseHtmlDOM();
-        String info = theme + '\n' + dataList.toString();
-        Main.setHtml(file, info);
-        //return null;
-    }
+    public abstract Extractable extractInformation(File file) throws IOException;
 
 }
