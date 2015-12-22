0933f122cea0cdcd52bf45a0a708c5c04b1e3a14
ZhangZhi <1557983850@qq.com>
2015-12-22 17:55:37 +0800

    create ExtractNews

--- InformationExtraction/src/cn/edu/njnu/infoextract/impl/ExtractNews.java
+++ InformationExtraction/src/cn/edu/njnu/infoextract/impl/ExtractNews.java
@@ -1,13 +1,24 @@
 package cn.edu.njnu.infoextract.impl;
 
+import cn.edu.njnu.Main;
+import cn.edu.njnu.domain.Extractable;
 import cn.edu.njnu.infoextract.InfoExtract;
 
+import java.io.File;
+import java.io.IOException;
+
 /**
- * Created by zhangzhi on 15-12-22.
+ * Created by zhangzhi on 15-12-21.
  * 新闻类型页面抽取实现类
  */
 public class ExtractNews extends InfoExtract {
-    public ExtractNews() {
 
+    @Override
+    public Extractable extractInformation(File file) throws IOException {
+        parseHtmlDOM();
+        String info = theme + '\n' + dataList.toString();
+        Main.setHtml(file, info);
+        return null;
     }
+
 }
