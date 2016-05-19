ebba8964ea64bffc3f09c852c4c4d025e36bfbeb
zshell.zhang <zshell.zhang@qunar.com>
2016-05-19 23:27:01 +0800

    update

---
+++ Information_Extraction/info-extract/src/main/java/cn/edu/njnu/extract/extract/InfoExtractService.java
@@ -0,0 +1,22 @@
+package cn.edu.njnu.extract.extract;
+
+import cn.edu.njnu.extract.domain.Extractable;
+import org.jsoup.nodes.Element;
+
+import java.util.List;
+
+/**
+ * Created by zhangzhi on 15-12-19.
+ * 定义信息抽取服务行为规范
+ */
+public interface InfoExtractService {
+
+    /**
+     * 从给定的网页结构中抽取出结构化的信息
+     *
+     * @param root 页面对应的DOM树
+     * @return 抽取出的数据单元list
+     */
+    List<Extractable> extract(Element root);
+
+}
---
+++ Information_Extraction/thematic-crawler/src/main/java/cn/edu/njnu/crawler/classify/PageClassifyService.java
@@ -0,0 +1,11 @@
+package cn.edu.njnu.crawler.classify;
+
+/**
+ * Created by zhangzhi on 16-5-7.
+ * 定义网页分类服务行为规范
+ */
+public interface PageClassifyService {
+
+
+
+}
