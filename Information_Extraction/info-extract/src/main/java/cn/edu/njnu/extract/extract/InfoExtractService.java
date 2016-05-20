58783ba69df9687df7ccd7b5387e47bf6964f619
zshell.zhang <zshell.zhang@qunar.com>
2016-05-20 08:37:12 +0800

    update

--- Information_Extraction/info-extract/src/main/java/cn/edu/njnu/extract/extract/InfoExtractService.java
+++ Information_Extraction/info-extract/src/main/java/cn/edu/njnu/extract/extract/InfoExtractService.java
@@ -2,6 +2,7 @@ package cn.edu.njnu.extract.extract;
 
 import cn.edu.njnu.extract.domain.Extractable;
 import org.jsoup.nodes.Element;
+import org.springframework.core.Ordered;
 
 import java.util.List;
 
@@ -9,9 +10,16 @@ import java.util.List;
  * Created by zhangzhi on 15-12-19.
  * 定义信息抽取服务行为规范
  */
-public interface InfoExtractService {
+public interface InfoExtractService extends Ordered {
 
-    boolean capableOf();
+    /**
+     * 判断所给参数是否匹配本类型抽取服务
+     *
+     * @param root     页面对应的DOM树
+     * @param category 类别
+     * @return 是否匹配本类型服务
+     */
+    boolean capableOf(Element root, String category);
 
     /**
      * 从给定的网页结构中抽取出结构化的信息
